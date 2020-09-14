b9a3646aa6eefa6dafc83d74cb5fd69126c72a51
Andrey Cherkashin <action.open@gmail.com>
2020-09-14 12:14:44 +0200

    + adding Mathematics extensions

---
+++ Extensions/MathematicsExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Unity.Mathematics;
+
+namespace Plugins.ECSEntityBuilder.Extensions
+{
+    public static class MathematicsExtensions
+    {
+        public static float3 ConvertTo3D(this int2 value)
+        {
+            return new float3(value.x, 0, value.y);
+        }
+
+        public static int2 ConvertTo2D(this float3 value)
+        {
+            return new int2((int) value.x, (int) value.z);
+        }
+    }
+}

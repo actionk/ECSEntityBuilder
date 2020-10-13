9f5041f14338c06887ee8037a3ca83a6be53b7a6
Andrey Cherkashin <action.open@gmail.com>
2020-10-13 14:16:37 +0200

    + adding an extension for NativeHashMap

---
+++ Extensions/NativeCollectionsExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Unity.Collections;
+
+namespace Plugins.ECSEntityBuilder.Extensions
+{
+    public static class NativeCollectionsExtensions
+    {
+        public static TValue GetValueOrDefault<TKey, TValue>(this NativeHashMap<TKey, TValue> hashMap, TKey key, TValue defaultValue)
+            where TKey : struct, IEquatable<TKey>
+            where TValue : struct
+        {
+            if (hashMap.TryGetValue(key, out TValue resultValue))
+                return resultValue;
+
+            return defaultValue;
+        }
+    }
+}

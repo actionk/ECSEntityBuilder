9ad60b4c860044f25609865e6e5ba2e4ea33dff4
Andrey Cherkashin <action.open@gmail.com>
2020-10-15 15:20:58 +0200

    + removing useless component

--- Systems/ManagedGameObjectSystem.cs
+++ Systems/ManagedGameObjectSystem.cs
@@ -7,31 +7,16 @@ namespace Plugins.ECSEntityBuilder.Systems
     [UpdateInGroup(typeof(InitializationSystemGroup))]
     public class ManagedGameObjectSystem : ComponentSystem
     {
-        private struct ManagedGameObjectSpawned : ISystemStateComponentData
-        {
-            public int instanceId;
-        }
-
         protected override void OnUpdate()
         {
             Entities
                 .WithAll<ManagedGameObject>()
-                .WithNone<ManagedGameObjectSpawned>()
                 .ForEach((Entity entity, ref ManagedGameObject managedGameObject) =>
                 {
                     var gameObject = GameObjectEntityManager.Instance[managedGameObject.instanceId];
                     EntityManager.AddComponentObject(entity, gameObject.transform);
                     gameObject.SetActive(true);
-                    PostUpdateCommands.AddComponent(entity, new ManagedGameObjectSpawned {instanceId = managedGameObject.instanceId});
-                });
-
-            Entities
-                .WithAll<ManagedGameObjectSpawned>()
-                .WithNone<ManagedGameObject>()
-                .ForEach((Entity entity, ref ManagedGameObjectSpawned managedGameObjectSpawned) =>
-                {
-                    GameObjectEntityManager.Instance.Destroy(managedGameObjectSpawned.instanceId);
-                    PostUpdateCommands.RemoveComponent<ManagedGameObjectSpawned>(entity);
+                    PostUpdateCommands.RemoveComponent<ManagedGameObject>(entity);
                 });
         }
     }

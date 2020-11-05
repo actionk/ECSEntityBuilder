982c600036f86d1833c39a8816af7143d524e5d9
Andrey Cherkashin <action.open@gmail.com>
2020-11-05 14:36:40 +0100

    + more dynamic extensions

--- Extensions/DynamicBufferExtensions.cs
+++ Extensions/DynamicBufferExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Unity.Entities;
+﻿using System;
+using Unity.Entities;
 
 namespace Plugins.ECSEntityBuilder.Extensions
 {
@@ -10,6 +11,26 @@ namespace Plugins.ECSEntityBuilder.Extensions
             for (var i = 0; i < array.Length; i++)
                 array[i] = buffer[i];
             return array;
+        }
+        
+        public static int IndexOf<T>(this DynamicBuffer<T> buffer, Predicate<T> predicate) where T : struct,IBufferElementData
+        {
+            for (var i = 0; i < buffer.Length; i++)
+            {
+                if (predicate.Invoke(buffer[i]))
+                    return i;
+            }
+
+            return -1;
+        }
+        
+        public static void AddOrReplace<T>(this DynamicBuffer<T> buffer, T element, Predicate<T> predicate) where T : struct,IBufferElementData
+        {
+            var indexOfExistingElement = buffer.IndexOf(predicate);
+            if (indexOfExistingElement >= 0)
+                buffer[indexOfExistingElement] = element;
+            else
+                buffer.Add(element);
         }
     }
 }

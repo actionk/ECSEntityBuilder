bc99a1ef5e5312471861f9b2e760ccf80880d6e6
Andrey Cherkashin <action.open@gmail.com>
2020-11-08 20:57:03 +0100

    + more extension methods

--- Extensions/DynamicBufferExtensions.cs
+++ Extensions/DynamicBufferExtensions.cs
@@ -5,15 +5,24 @@ namespace Plugins.ECSEntityBuilder.Extensions
 {
     public static class DynamicBufferExtensions
     {
-        public static T[] ToArray<T>(this DynamicBuffer<T> buffer) where T : struct,IBufferElementData
+        public static T[] ToArray<T>(this DynamicBuffer<T> buffer) where T : struct, IBufferElementData
         {
             var array = new T[buffer.Length];
             for (var i = 0; i < array.Length; i++)
                 array[i] = buffer[i];
             return array;
         }
-        
-        public static int IndexOf<T>(this DynamicBuffer<T> buffer, Predicate<T> predicate) where T : struct,IBufferElementData
+
+        public static T2[] ToArray<T, T2>(this DynamicBuffer<T> buffer, Func<T, T2> mappingFunction) where T : struct, IBufferElementData
+        {
+            var array = new T2[buffer.Length];
+            for (var i = 0; i < array.Length; i++)
+                array[i] = mappingFunction.Invoke(buffer[i]);
+
+            return array;
+        }
+
+        public static int IndexOf<T>(this DynamicBuffer<T> buffer, Predicate<T> predicate) where T : struct, IBufferElementData
         {
             for (var i = 0; i < buffer.Length; i++)
             {
@@ -23,8 +32,8 @@ namespace Plugins.ECSEntityBuilder.Extensions
 
             return -1;
         }
-        
-        public static void AddOrReplace<T>(this DynamicBuffer<T> buffer, T element, Predicate<T> predicate) where T : struct,IBufferElementData
+
+        public static void AddOrReplace<T>(this DynamicBuffer<T> buffer, T element, Predicate<T> predicate) where T : struct, IBufferElementData
         {
             var indexOfExistingElement = buffer.IndexOf(predicate);
             if (indexOfExistingElement >= 0)
